b0cd3ea393132ef4b9a21425888db83bbfc374b5
Meery Awad <meeryawad19@gmail.com>
2025-09-20 13:06:44 +0400

    index edit

--- index.js
+++ index.js
@@ -29,7 +29,13 @@ const deleteAllCourses = async () => {
   }
 };
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 mongoose.connect(process.env.MONGO_URI)
