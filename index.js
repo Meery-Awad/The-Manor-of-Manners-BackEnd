e5d87d512e640e3d8a5ea1a6b8969f5834f31038
Meery Awad <meeryawad19@gmail.com>
2025-09-22 09:51:42 +0400

    hosting edit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const deleteAllCourses = async () => {
 };
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
