426701deaa9d5cb351652118c45ae2a0f3541839
Meery-Awad <84231943+Meery-Awad@users.noreply.github.com>
2025-09-29 09:47:45 +0300

    Update paymentController.js
    add `

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -98,7 +98,7 @@ exports.createCheckoutSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: https://the-manor-of-manners.netlify.app/Success?courseId=${courseId}`,
+      success_url: `https://the-manor-of-manners.netlify.app/Success?courseId=${courseId}`,
       cancel_url: "https://the-manor-of-manners.netlify.app/cancel",
     });
 
